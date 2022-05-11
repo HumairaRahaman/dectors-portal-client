e6e9347b4a48dc688bd2f591369f3f7f9d641690
Humaira Rahaman <humairamitul@gmail.com>
2022-05-11 23:44:06 +0600

    navbar ,banner,info cart

---
+++ src/Pages/Home/InfoCart.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const InfoCart = ({img,cartTitle,cartDes,bgClass}) => {
+    return (
+        <div className={`card lg:card-side bg-base-100 shadow-xl ${bgClass}`}>
+  <figure className=' pl-5'><img src={img} alt="Album"/></figure>
+  <div className="card-body">
+    <h2 className="card-title text-white">{cartTitle}</h2>
+    <p className=' text-white'>{cartDes}</p>
+   
+  </div>
+</div>
+    );
+};
+
+export default InfoCart;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,26 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'background-img': "url('/src/assets/images/banner-bg.png')",
+        
+      }
+    },
+  },daisyui: {
+    themes: [
+      {
+        doctortheme: {
+          primary: "#0fcfec",
+          secondary: "#19d3ae",
+          accent: "#3a4256",
+          neutral: "#3d4451",
+          "base-100": "#ffffff",
+        },
+      },
+      
+    ],
   },
+
   plugins: [require("daisyui")],
 }
