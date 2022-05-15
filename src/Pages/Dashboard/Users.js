bb8be3c15f5828d9dea43b2b651dbdbfcca01891
Humaira Rahaman <humairamitul@gmail.com>
2022-05-15 15:32:48 +0600

    admin created

--- src/Pages/Dashboard/Users.js
+++ src/Pages/Dashboard/Users.js
@@ -1,20 +1,24 @@
-import React from "react"
+import React, { useEffect } from "react"
 import { useQuery } from "react-query"
 import Loading from "../Shared/Loading"
 import UserRow from "./UserRow"
 
 const Users = () => {
-  const { data: users, isLoading } = useQuery("users", () =>
-    fetch("http://localhost:5000/user",{
-        method: 'GET',
-        headers:{
-            authorization: `Bearer ${localStorage.getItem('accessToken')}`
-        }
+  const { data: users, isLoading, refetch } = useQuery("users", () =>
+    fetch("http://localhost:5000/user", {
+      method: "GET",
+      headers: {
+        authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+      },
     }).then((res) => res.json())
   );
+  useEffect(() => {
+    console.log(users);
+  }, [users]);
   if (isLoading) {
     return <Loading></Loading>;
   }
+
   return (
     <div>
       <h2> All Users: {users.length}</h2>
@@ -29,7 +33,9 @@ const Users = () => {
             </tr>
           </thead>
           <tbody>
-           {users.map((user,index)=><UserRow index={index} key={user._id} user={user}></UserRow>)}
+            {users.map((user, index) => (
+              <UserRow refetch={refetch} index={index} key={user._id} user={user}></UserRow>
+            ))}
           </tbody>
         </table>
       </div>
